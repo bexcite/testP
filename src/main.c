2e949299a4c216f0cee556085edf6d729c882d82
Pavel Bashmakov <pavel.bashmakov@gmail.com>
2014-05-03 10:43:47 -0700

    some

--- src/main.c
+++ src/main.c
@@ -24,16 +24,15 @@ static void config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_SELECT, &select_click_handler);
   window_single_click_subscribe(BUTTON_ID_UP, &up_click_handler);
   window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
-  window_single_click_subscribe(BUTTON_ID_BACK, back_click_handler);
+  //window_single_click_subscribe(BUTTON_ID_BACK, back_click_handler);
 }
 
 static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
   
   char *str;
-  if (units_changed == MINUTE_UNIT) {
+  if (units_changed && MINUTE_UNIT) {
     APP_LOG(APP_LOG_LEVEL_DEBUG, "Set Minutes");
     str = "Minute";
-    
   } else {
     APP_LOG(APP_LOG_LEVEL_DEBUG, "Set Else");
     str = "else ...";
@@ -46,11 +45,24 @@ static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
 
 void init(void) {
   window = window_create();
-  window_stack_push(window, true);
+  window_stack_push(window, false);
   window_set_click_config_provider(window, &config_provider);
   
 
-  text_layer = text_layer_create(GRect(0, 0, 144, 20));
+  Layer* window_layer = window_get_root_layer(window);
+  
+  
+  text_layer = text_layer_create(GRect(0, 0, 144, 144));
+  text_layer_set_background_color(text_layer, GColorWhite);
+  
+  //Get some info about layers
+  GRect bounds = layer_get_bounds(text_layer_get_layer(text_layer));
+  char *log_str = (char *) malloc(sizeof(char) * 200);
+  snprintf(log_str, 200, "text_layer.bounds = x%d y%d w%d h%d", bounds.origin.x, bounds.origin.y, bounds.size.w, bounds.size.h);
+//  APP_LOG(APP_LOG_LEVEL_DEBUG, "text_layer.bounds = x%d y%d", bounds.origin.x, bounds.origin.y);
+  
+  text_layer_set_text(text_layer, log_str);
+  layer_add_child(window_layer, text_layer_get_layer(text_layer));
 
   
   tick_timer_service_subscribe(MINUTE_UNIT, &handle_minute_tick);
